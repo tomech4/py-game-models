a468920c75e19d69d097637b18227a614f2456ef
tomech <109527747+tomech4@users.noreply.github.com>
2026-03-10 20:53:44 +0100

    implement Player model

--- db/models.py
+++ db/models.py
@@ -16,3 +16,12 @@ class Guild(models.Model):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True)
 
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="players")
+    guild = models.ForeignKey(Guild, on_delete=models.DO_NOTHING)
+    created_at = models.DateField(auto_now_add=True)
+
