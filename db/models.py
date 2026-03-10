fa22e9037a3e4fa7fadfe395ddeb833246e7e1c8
tomech <109527747+tomech4@users.noreply.github.com>
2026-03-10 20:45:49 +0100

    refactor cascade dependency

--- db/models.py
+++ db/models.py
@@ -9,5 +9,5 @@ class Race(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=CASCADE, related_name="skills")
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="skills")
 
